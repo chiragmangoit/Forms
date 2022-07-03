5f67c1f4add213377032873ef22acdcc20bfcb9f
Chirag <chirag.mangoit@gmail.com>
2022-07-04 01:05:23 +0530

    home

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { UserService } from '../user.service';
 import { CustomValidations } from './custom-validations';
 
 @Component({
@@ -8,24 +11,153 @@ import { CustomValidations } from './custom-validations';
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-  status: [string, string, string] = ['stable', 'critical', 'finished'];
-  project: FormGroup;
+  regForm:FormGroup;
+  editMode: boolean = false;
+  id: number;
+  dropdownList: string[] = [];
+  dropdownSettings: IDropdownSettings = {};
+  genders: string[] = ['male', 'female'];
+  hobbies: any = [
+    {
+      id: 1,
+      name: 'singing',
+      selected: false,
+    },
+    {
+      id: 2,
+      name: 'dancing',
+      selected: false,
+    },
+    {
+      id: 3,
+      name: 'reading',
+      selected: false,
+    },
+  ];
+  profession: string[] = [
+    'Chiropractor',
+    'Dentist',
+    'Optometrist',
+    'Developer',
+    'Tester',
+  ];
+  contact: any = [
+    {
+      id: 1,
+      contactName: '',
+      contactNumber: '',
+    },
+  ];
 
-  constructor() {}
+  user: object = {
+    name: '',
+    email: '',
+    gender: '',
+    dob: '',
+    dp: '',
+    hobbies: [],
+    phoneNum: '',
+    qualification: [],
+    profession: '',
+    description: '',
+    contacts: [] ,
+  };
+
+  constructor(
+    private userDataService: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.project = new FormGroup({
-      projectName: new FormControl(
-        null,
-        [Validators.required, CustomValidations.invalidProjectName],
-        CustomValidations.asyncInvalidProjectName
-      ),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      status: new FormControl('stable'),
+    this.dropdownList = ['MCA', 'BCA', 'B.tech', 'M.tech', 'B.Com'];
+    this.dropdownSettings = {
+      idField: 'item_id',
+      textField: 'item_text',
+      enableCheckAll: false,
+    };
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      this.initForm();
+    });
+
+    this.regForm = new FormGroup({
+      userDataOne: new FormGroup({
+        name: new FormControl(null,Validators.required),
+        email: new FormControl(null,[Validators.required, Validators.email]),
+        gender: new FormControl(null,Validators.required),
+        dob: new FormControl(null,Validators.required)
+      }),
+      userDataTwo:new FormGroup({
+        dp:new FormControl(null, Validators.required),
+        hobbies:new FormArray(null, Validators.required),
+        phoneNum:new FormControl(null, Validators.required),
+      }),
+      userDataThree:new FormGroup({
+        qualification:new FormArray(null, Validators.required),
+        description:new FormControl(null, Validators.required),
+        contacts:new FormArray(null, Validators.required),
+      })
+    })
+  }
+
+  initForm() {
+    if (this.editMode) {
+      const newUserData = this.userDataService.getUserData(this.id);
+      const selectedHobby: string[] = newUserData['hobbies'];
+      this.fetchSelectedHobby(selectedHobby);
+      this.user['name'] = newUserData['name'];
+      this.user['email'] = newUserData['email'];
+      this.user['gender'] = newUserData['gender'];
+      this.user['dob'] = newUserData['dob'];
+      this.user['phoneNum'] = newUserData['phoneNum'];
+      this.user['qualification'] = newUserData['qualification'];
+      this.user['profession'] = newUserData['profession'];
+      this.user['description'] = newUserData['description'];
+      this.contact = newUserData['contacts'];
+    }
+  }
+
+  getSelectedHobby(): string[] {
+    const selectedHobby = [];
+    for (let e of this.hobbies) {
+      if (this.regForm.value.userDataTwo[e.id]) {
+        selectedHobby.push(e.name);
+      }
+    }
+    return selectedHobby;
+  }
+
+  fetchSelectedHobby(selectedHobby: string[]) {
+    for (let e of selectedHobby) {
+      for (let f of this.hobbies) {
+        if (e == f['name']) {
+          f['selected'] = true;
+        }
+      }
+    }
+  }
+
+  onAddContacts() {
+    this.contact.push({
+      id: this.contact.length + 1,
+      contactName: '',
+      contactNumber: '',
     });
   }
 
-  onSaveProject() {
-    console.log(this.project.value);
+  onRemoveContacts(index: number) {
+    this.contact.splice(index, 1);
+  }
+  onSubmit() {
+    this.user['hobbies'] = this.getSelectedHobby();
+    this.user['contacts'] = this.contact
+    if (!this.editMode) {
+      this.userDataService.addData(this.user);
+    } else {
+      this.userDataService.updateData(this.id, this.user)
+    }
+    this.router.navigate(['/home'], { relativeTo: this.route });
   }
 }
